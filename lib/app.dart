d6aa67a81a615e8b8acd9ba4474ea285ae3a6f35
icodelifee <mithunsmenon@gmail.com>
2021-02-26 19:51:24 +0530

    feat(font): add google font and use poppins

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rcmasbusapp/ui/login/login_page.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'RCMAS Bus App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
       ),
       defaultTransition: Transition.cupertino,
       home: LoginPage(),
