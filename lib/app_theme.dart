8844817b67542d33d16628a60c7ce1f43c886561
icodelifee <mithunsmenon@gmail.com>
2021-02-26 19:51:51 +0530

    style: refactor theme file

---
+++ lib/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+final border = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+    borderSide: BorderSide(
+      color: Colors.transparent,
+    ));
+
+var roundedRectangleBorder =
+    RoundedRectangleBorder(borderRadius: BorderRadius.circular(10));
