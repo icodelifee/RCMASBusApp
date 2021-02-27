796f0c2983af24c5ed15773be93b855de2ded6ac
icodelifee <mithunsmenon@gmail.com>
2021-02-27 19:59:32 +0530

    core: intialize firebase app and providerscope in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:rcmasbusapp/app.dart';
 
-void main() {
-  runApp(App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(ProviderScope(child: App()));
 }
-
