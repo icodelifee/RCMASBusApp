6318bc7f7acc4e7c0258e9ae0be5c96079a35dff
icodelifee <mithunsmenon@gmail.com>
2021-02-27 19:58:57 +0530

    feat: implement route locator for auth

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:rcmasbusapp/route_locator.dart';
 import 'package:rcmasbusapp/ui/login/login_page.dart';
 
 class App extends StatelessWidget {
+  const App({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
@@ -17,7 +19,7 @@ class App extends StatelessWidget {
         ),
       ),
       defaultTransition: Transition.cupertino,
-      home: LoginPage(),
+      home: RouteLocator(),
     );
   }
 }
---
+++ lib/route_locator.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:rcmasbusapp/ui/login/login_page.dart';
+import 'package:rcmasbusapp/ui/home/home_page.dart';
+
+class RouteLocator extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          final user = snapshot.data;
+          if (user == null) {
+            return LoginPage();
+          } else {
+            return HomePage();
+          }
+        } else {
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
