a0dcbb2fd4456a22ed8c60687477edea515ac4ea
icodelifee <mithunsmenon@gmail.com>
2021-03-01 22:33:54 +0530

    feat: create loginuser model

---
+++ lib/data/model/login_user.dart
@@ -0,0 +1,23 @@
+class LoginUser {
+  final String firstName;
+  final String lastName;
+  final String phone;
+  final bool regComplete;
+  final String rollNumber;
+  final String userType;
+
+  LoginUser(
+      {this.firstName,
+      this.lastName,
+      this.phone,
+      this.regComplete,
+      this.rollNumber,
+      this.userType});
+  LoginUser.fromJson(Map<String, dynamic> json)
+      : firstName = json['first_name'],
+        lastName = json['last_name'],
+        phone = json['phone'],
+        regComplete = json['reg_complete'],
+        rollNumber = json['roll_no'],
+        userType = json['user_type'];
+}
