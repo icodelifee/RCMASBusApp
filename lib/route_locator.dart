5812729bb7bafaac45b6cb12016602e74b2360db
icodelifee <mithunsmenon@gmail.com>
2021-03-01 22:58:30 +0530

    style: refactor loading page in route locator

--- lib/route_locator.dart
+++ lib/route_locator.dart
@@ -4,6 +4,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:rcmasbusapp/data/provider/auth_changes_provider.dart';
 import 'package:rcmasbusapp/data/provider/firestore_repository_provider.dart';
+import 'package:rcmasbusapp/ui/components/loading_page.dart';
 import 'package:rcmasbusapp/ui/login/login_page.dart';
 import 'package:rcmasbusapp/ui/home/home_page.dart';
 import 'package:rcmasbusapp/ui/registration/registration_page.dart';
@@ -13,38 +14,25 @@ class RouteLocator extends HookWidget {
   Widget build(BuildContext context) {
     final fetchUser = context.read(fireStoreRepositoryProvider).getLoginUser;
     final authChangeProvider = useProvider(authChangesProvider);
-    return authChangeProvider.data.when(data: (User user) {
-      if (user == null) {
-        return LoginPage();
-      } else {
-        final userFuture = useMemoized(fetchUser);
-        final loginUser = useFuture(userFuture);
-        if (loginUser.hasData) {
-          if (loginUser.data.regComplete ?? false) {
-            return HomePage();
+    return authChangeProvider.data.when(
+        data: (User user) {
+          if (user == null) {
+            return LoginPage();
           } else {
-            return RegistrationPage();
+            final userFuture = useMemoized(fetchUser);
+            final loginUser = useFuture(userFuture);
+            if (loginUser.hasData) {
+              if (loginUser.data.regComplete ?? false) {
+                return HomePage();
+              } else {
+                return RegistrationPage();
+              }
+            } else {
+              return LoadingPage();
+            }
           }
-        } else {
-          return Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        }
-      }
-    }, loading: () {
-      return Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
-    }, error: (_, __) {
-      return Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
-    });
+        },
+        loading: () => LoadingPage(),
+        error: (_, __) => LoadingPage());
   }
 }
