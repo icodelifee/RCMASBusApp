6744bb7f8f6d9c8e710e88c00e152c4aa7765a86
icodelifee <mithunsmenon@gmail.com>
2021-03-28 09:57:14 +0530

    feat: add registration payment button

---
+++ lib/ui/registration/Widgets/payment_submit_btn.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:get/get.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:rcmasbusapp/data/model/login_user.dart';
+import 'package:rcmasbusapp/ui/components/snackbar.dart';
+import 'package:rcmasbusapp/ui/registration/registration_page_viewmodel.dart';
+import 'package:rcmasbusapp/ui/registration/registration_payment_page.dart';
+
+import '../../../app_theme.dart';
+
+class PaymentSubmitButton extends HookWidget {
+  const PaymentSubmitButton({
+    Key? key,
+    required this.formKey,
+  }) : super(key: key);
+
+  final GlobalKey<FormState> formKey;
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = useProvider(registrationPageViewModelProvider);
+    return TextButton(
+      onPressed: () {
+        if (formKey.currentState!.validate()) {
+          
+        } else {
+          showSnackbar('Please fill all the fields',
+              'All Fields must be filled to procced');
+        }
+      },
+      style: btnStyle(),
+      child: SizedBox(
+        width: double.infinity,
+        height: 50,
+        child: !provider.isLoading
+            ? btnText()
+            : Center(child: CircularProgressIndicator(color: Colors.white)),
+      ),
+    );
+  }
+
+  ButtonStyle btnStyle() {
+    return ButtonStyle(
+      shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))),
+      backgroundColor: MaterialStateProperty.all(AppColors.buttonColor),
+    );
+  }
+
+  Row btnText() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Submit',
+          style: TextStyle(
+              fontSize: 20, color: Colors.white, fontWeight: FontWeight.w900),
+        ),
+        Icon(
+          Icons.arrow_forward_rounded,
+          color: Colors.white,
+        )
+      ],
+    );
+  }
+}
