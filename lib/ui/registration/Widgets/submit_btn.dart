16202a72e2acfa4355c0c6f67b376f74c2f93116
icodelifee <mithunsmenon@gmail.com>
2021-03-28 09:55:55 +0530

    feat: add submit button in reg bus route page

---
+++ lib/ui/registration/Widgets/submit_btn.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:get/get.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:rcmasbusapp/data/model/login_user.dart';
+import 'package:rcmasbusapp/ui/components/snackbar.dart';
+import 'package:rcmasbusapp/ui/registration/registration_page_viewmodel.dart';
+import 'package:rcmasbusapp/ui/registration/registration_payment_page.dart';
+
+import '../../../app_theme.dart';
+
+class SubmitButton extends HookWidget {
+  const SubmitButton({
+    Key? key,
+    required this.formKey,
+    required LoginUser user,
+  })   : _user = user,
+        super(key: key);
+
+  final GlobalKey<FormState> formKey;
+  final LoginUser _user;
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = useProvider(registrationPageViewModelProvider);
+    return TextButton(
+      onPressed: () {
+        provider.isLoading = true;
+        if (formKey.currentState!.validate()) {
+          provider
+              .saveUserData(_user)
+              .then((value) => Get.offAll(() => RegistrationPaymentPage()));
+        } else {
+          provider.isLoading = false;
+          showSnackbar('Please fill all the fields',
+              'All Fields must be filled to procced');
+        }
+      },
+      style: btnStyle(),
+      child: SizedBox(
+        width: double.infinity,
+        height: 50,
+        child: !provider.isLoading
+            ? btnText()
+            : Center(child: CircularProgressIndicator(color: Colors.white)),
+      ),
+    );
+  }
+
+  ButtonStyle btnStyle() {
+    return ButtonStyle(
+      shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))),
+      backgroundColor: MaterialStateProperty.all(AppColors.buttonColor),
+    );
+  }
+
+  Row btnText() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Submit',
+          style: TextStyle(
+              fontSize: 20, color: Colors.white, fontWeight: FontWeight.w900),
+        ),
+        Icon(
+          Icons.arrow_forward_rounded,
+          color: Colors.white,
+        )
+      ],
+    );
+  }
+}
