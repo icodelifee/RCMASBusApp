58016a7bf37f847adde7baa522497919e2e49045
icodelifee <mithunsmenon@gmail.com>
2021-03-28 09:47:03 +0530

    style: refactor progress indicator

---
+++ lib/ui/registration/Widgets/progress_indicator.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:rcmasbusapp/app_theme.dart';
+
+class CustomIndicator extends StatelessWidget {
+  const CustomIndicator({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: CircularProgressIndicator(
+      color: AppColors.accentColor,
+    ));
+  }
+}
