283714a0300113f4ee19777b4e7abeb909cb6a71
icodelifee <mithunsmenon@gmail.com>
2021-03-28 09:54:12 +0530

    feat: add registration page methods to firestore data source

--- lib/data/remote/firestore_data_source.dart
+++ lib/data/remote/firestore_data_source.dart
@@ -3,4 +3,8 @@ import 'package:rcmasbusapp/data/model/login_user.dart';
 abstract class FireStore {
   Future<bool> checkUserEntry(String phone);
   Future<LoginUser> getLoginUser();
+  Future getBusRoutes();
+  Future<List<Map>> getBusStops(String? route);
+  Future<void> saveUserData(Map<String, dynamic> data);
+  Future<void> savePayment(String payCode);
 }
--- lib/data/remote/firestore_data_source_impl.dart
+++ lib/data/remote/firestore_data_source_impl.dart
@@ -1,7 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:logger/logger.dart';
 import 'package:rcmasbusapp/data/model/login_user.dart';
 import 'package:rcmasbusapp/data/remote/firestore_data_source.dart';
 
@@ -9,7 +7,7 @@ class FireStoreImpl implements FireStore {
   FireStoreImpl({required auth}) : _auth = auth;
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
   final FirebaseAuth _auth;
-  final Logger _logger = Logger();
+  // final Logger _logger = Logger();
   @override
   Future<bool> checkUserEntry(String phone) async {
     final qs = await firestore
@@ -26,7 +24,53 @@ class FireStoreImpl implements FireStore {
         .where('phone',
             isEqualTo: _auth.currentUser!.phoneNumber!.replaceAll('+', ''))
         .get();
-    final user = LoginUser.fromJson(qs.docs.first.data()!);
+    final user = LoginUser.fromJson(qs.docs.first.data()!, qs.docs.first.id);
     return user;
+  }
+
+  @override
+  Future getBusRoutes() async {
+    final qs = await firestore.collection('routes').get();
+    return qs.docs.map((e) {
+      final data = e.data();
+      data!.putIfAbsent('doc', () => e.id);
+      return data;
+    }).toList();
+  }
+
+  @override
+  Future<List<Map>> getBusStops(String? route) async {
+    if (route == null) return [];
+    final qs = await firestore
+        .collection('routes')
+        .doc(route)
+        .collection('stops')
+        .get();
+    return qs.docs.map((e) {
+      final data = e.data();
+      data!.putIfAbsent('doc', () => e.id);
+      return data;
+    }).toList();
+  }
+
+  @override
+  Future<void> saveUserData(Map<String, dynamic> data) async {
+    await firestore.collection('students').add(data);
+    await firestore
+        .collection('login')
+        .where('roll_no', isEqualTo: data['roll_no'])
+        .get()
+        .then((value) async {
+      final id = value.docs.first.id;
+      await firestore
+          .collection('login')
+          .doc(id)
+          .update({'reg_complete': true});
+    });
+  }
+
+  @override
+  Future<void> savePayment(String payCode) async {
+    // await firestore.collection('students').a
   }
 }
