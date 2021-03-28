1fd467ab0aa02ada3da93f5dae6f07f963e20c90
icodelifee <mithunsmenon@gmail.com>
2021-03-28 09:51:21 +0530

    feat: use form and refactor registration page

--- lib/ui/registration/registration_page.dart
+++ lib/ui/registration/registration_page.dart
@@ -1,155 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:gap/gap.dart';
-import 'package:rcmasbusapp/app_theme.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:rcmasbusapp/data/model/login_user.dart';
+import 'package:rcmasbusapp/ui/registration/Widgets/course_selector.dart';
+import 'package:rcmasbusapp/ui/registration/Widgets/disabled_text_field.dart';
+import 'package:rcmasbusapp/ui/registration/Widgets/email_text_field.dart';
+import 'package:rcmasbusapp/ui/registration/Widgets/registration_appbar.dart';
+import 'package:rcmasbusapp/ui/registration/Widgets/semester_selector.dart';
+import 'package:rcmasbusapp/ui/registration/Widgets/text_field_label.dart';
+import 'package:rcmasbusapp/ui/registration/registration_page_viewmodel.dart';
+import 'Widgets/next_btn.dart';
 
 class RegistrationPage extends HookWidget {
   RegistrationPage({required user}) : _user = user;
   final LoginUser? _user;
-  final List<String> semesters = ['I', 'II', 'III', 'IV', 'V', 'VI'];
   @override
   Widget build(BuildContext context) {
-    final TextEditingController? email = useTextEditingController();
-    final TextEditingController? course = useTextEditingController();
+    final provider = useProvider(registrationPageViewModelProvider);
+    provider.email = useTextEditingController();
     return Scaffold(
-      appBar: appBar(),
+      appBar: RegistrationAppBar(),
       body: Container(
         padding: EdgeInsets.all(15),
-        child: ListView(
-          shrinkWrap: true,
-          children: [
-            Gap(10),
-            textFieldLabel('First name'),
-            disabledTextField('First name', _user!.firstName),
-            Gap(15),
-            textFieldLabel('Last name'),
-            disabledTextField('Last name', _user!.lastName),
-            Gap(15),
-            textFieldLabel('Roll number'),
-            disabledTextField('Roll number', _user!.rollNumber),
-            Gap(15),
-            textFieldLabel('Phone'),
-            disabledTextField('Phone number', _user!.phone),
-            Gap(15),
-            textFieldLabel('Email *'),
-            TextField(
-              controller: email,
-              decoration: InputDecoration(
-                  border: border,
-                  focusedBorder: border,
-                  enabledBorder: border,
-                  filled: true,
-                  fillColor: AppColors.textInputFillColor,
-                  hintText: 'Email'),
-            ),
-            Gap(15),
-            textFieldLabel('Course *'),
-            TextField(
-              controller: course,
-              decoration: InputDecoration(
-                  border: border,
-                  focusedBorder: border,
-                  enabledBorder: border,
-                  filled: true,
-                  fillColor: AppColors.textInputFillColor,
-                  hintText: 'Course'),
-            ),
-            Gap(15),
-            textFieldLabel('Semester *'),
-            semesterSelector(),
-            Gap(15),
-            nextButton()
-          ],
+        child: Form(
+          key: provider.formKey,
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              Gap(10),
+              TextFieldLabel(label: 'First name'),
+              DisabledTextField(hint: 'First name', text: _user!.firstName),
+              Gap(15),
+              TextFieldLabel(label: 'Last name'),
+              DisabledTextField(hint: 'Last name', text: _user!.lastName),
+              Gap(15),
+              TextFieldLabel(label: 'Roll number'),
+              DisabledTextField(hint: 'Roll number', text: _user!.rollNumber),
+              Gap(15),
+              TextFieldLabel(label: 'Phone'),
+              DisabledTextField(hint: 'Phone number', text: _user!.phone),
+              Gap(15),
+              TextFieldLabel(label: 'Email *'),
+              EmailTextField(),
+              Gap(15),
+              TextFieldLabel(label: 'Course *'),
+              CourseSelector(),
+              Gap(15),
+              TextFieldLabel(label: 'Semester *'),
+              SemesterSelector(),
+              Gap(15),
+              NextButton(
+                user: _user!,
+              )
+            ],
+          ),
         ),
-      ),
-    );
-  }
-
-  DropdownButtonFormField<String> semesterSelector() {
-    return DropdownButtonFormField(
-      hint: Text('Select semester'),
-      items: semesters
-          .map((e) => DropdownMenuItem(
-                child: Text(e),
-                value: e,
-              ))
-          .toList(),
-      onChanged: (val) {},
-      decoration: InputDecoration(
-          border: border,
-          enabledBorder: border,
-          focusedBorder: border,
-          fillColor: AppColors.textInputFillColor,
-          filled: true),
-    );
-  }
-
-  TextField disabledTextField(String hint, String? text) {
-    return TextField(
-      controller: TextEditingController(text: text ?? ''),
-      decoration: InputDecoration(
-          border: border,
-          enabled: false,
-          focusedBorder: border,
-          disabledBorder: border,
-          enabledBorder: border,
-          filled: true,
-          fillColor: AppColors.textInputFillColor,
-          hintText: hint),
-    );
-  }
-
-  TextButton nextButton() {
-    return TextButton(
-      onPressed: () {},
-      style: ButtonStyle(
-        shape: MaterialStateProperty.all(
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))),
-        backgroundColor: MaterialStateProperty.all(AppColors.buttonColor),
-      ),
-      child: SizedBox(
-        width: double.infinity,
-        height: 50,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Next',
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w900),
-            ),
-            Icon(
-              Icons.arrow_forward_rounded,
-              color: Colors.white,
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-  AppBar appBar() {
-    return AppBar(
-      title: Text(
-        'Registration',
-        style: TextStyle(
-            color: Colors.black, fontWeight: FontWeight.bold, fontSize: 26),
-      ),
-      backgroundColor: Colors.transparent,
-      elevation: 0,
-    );
-  }
-
-  Padding textFieldLabel(String text) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 2, bottom: 5),
-      child: Text(
-        text,
-        style: TextStyle(fontSize: 17, fontWeight: FontWeight.w700),
       ),
     );
   }
