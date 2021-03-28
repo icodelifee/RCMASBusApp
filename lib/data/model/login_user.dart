463371feaf76c92071349a33669cdc2f5e9ff0ab
icodelifee <mithunsmenon@gmail.com>
2021-03-28 09:58:58 +0530

    feat: add payComplete and doc id to login user model

--- lib/data/model/login_user.dart
+++ lib/data/model/login_user.dart
@@ -5,19 +5,25 @@ class LoginUser {
   final bool? regComplete;
   final String? rollNumber;
   final String? userType;
+  final bool? payComplete;
+  final String? docId;
 
   LoginUser(
       {this.firstName,
       this.lastName,
       this.phone,
+      this.docId,
       this.regComplete,
       this.rollNumber,
+      this.payComplete,
       this.userType});
-  LoginUser.fromJson(Map<String, dynamic> json)
+  LoginUser.fromJson(Map<String, dynamic> json, String docid)
       : firstName = json['first_name'],
         lastName = json['last_name'],
+        docId = docid,
         phone = json['phone'],
         regComplete = json['reg_complete'],
         rollNumber = json['roll_no'],
+        payComplete = json['pay_complete'],
         userType = json['user_type'];
 }
