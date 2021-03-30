55f7da9c33e3e6612463e815d852a9eb9e823f75
icodelifee <mithunsmenon@gmail.com>
2021-03-30 18:44:59 +0530

    ui: change color of progress indicator to accent color

--- lib/ui/registration/Widgets/progress_indicator.dart
+++ lib/ui/registration/Widgets/progress_indicator.dart
@@ -10,7 +10,7 @@ class CustomIndicator extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: CircularProgressIndicator(
-      color: AppColors.accentColor,
+      valueColor: AlwaysStoppedAnimation<Color>(AppColors.accentColor),
     ));
   }
 }
