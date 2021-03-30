1b9d39f3645f19cfa9005601d332472f79638912
icodelifee <mithunsmenon@gmail.com>
2021-03-30 18:44:05 +0530

    style: docRefContainer -> userContainer

--- lib/data/containers/user_container.dart
+++ lib/data/containers/user_container.dart
@@ -1,4 +1,3 @@
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:kiwi/kiwi.dart';
 
-KiwiContainer docRefContainer = KiwiContainer();
+KiwiContainer userContainer = KiwiContainer();
