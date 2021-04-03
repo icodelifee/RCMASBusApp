25b0bd19d87c2ca40dc6970d4d51bbf448d59128
icodelifee <mithunsmenon@gmail.com>
2021-04-03 12:26:42 +0530

    feat: add yourbusprovider for fetching your bus details

--- lib/ui/home/home_page_viewmodel.dart
+++ lib/ui/home/home_page_viewmodel.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:rcmasbusapp/data/model/bus_pass.dart';
+import 'package:rcmasbusapp/data/model/your_bus.dart';
 import 'package:rcmasbusapp/data/provider/firestore_repository_provider.dart';
 import 'package:rcmasbusapp/data/repository/firestore_repository.dart';
 
@@ -9,6 +10,9 @@ final homePageProvider = ChangeNotifierProvider(
 
 final busPassProvider = FutureProvider<BusPass>(
     (ref) async => await ref.watch(fireStoreRepositoryProvider).getBusPass());
+
+final yourBusProvider = FutureProvider<YourBus>(
+    (ref) async => await ref.watch(fireStoreRepositoryProvider).yourBusData());
 
 class HomePageViewModel extends ChangeNotifier {
   HomePageViewModel({required this.repo});
