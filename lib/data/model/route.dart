1fe5aae229fea8804ada547a7ceaaa5dfc427a40
icodelifee <mithunsmenon@gmail.com>
2021-04-03 12:23:11 +0530

    feat: add route

---
+++ lib/data/model/route.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rcmasbusapp/data/model/stop.dart';
+
+class Route {
+  Route(
+      {this.routeId,
+      this.routeLocation,
+      this.routeName,
+      this.stops,
+      this.docId});
+
+  String? routeId;
+  String? routeLocation;
+  String? routeName;
+  String? docId;
+  List<Stop>? stops;
+  factory Route.fromJson(Map<String, dynamic> json,
+          List<QueryDocumentSnapshot> stops, String docId) =>
+      Route(
+          routeId: json['route_id'],
+          routeLocation: json['route_location'],
+          routeName: json['route_name'],
+          stops: stops
+              .map((QueryDocumentSnapshot e) => Stop.fromJson(e.data()!, e.id))
+              .toList(),
+          docId: docId);
+
+  Map<String, dynamic> toJson() => {
+        'route_id': routeId,
+        'route_location': routeLocation,
+        'route_name': routeName,
+      };
+}
