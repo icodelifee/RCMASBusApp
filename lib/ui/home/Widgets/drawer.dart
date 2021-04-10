b7c59169aa6632e08c990ce3a69bef7d14f631d2
icodelifee <mithunsmenon@gmail.com>
2021-04-10 20:26:05 +0530

    feat: clear container when signing out

--- lib/ui/home/Widgets/drawer.dart
+++ lib/ui/home/Widgets/drawer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:rcmasbusapp/data/containers/user_container.dart';
+import 'package:rcmasbusapp/data/model/login_user.dart';
 import 'package:rcmasbusapp/ui/home/home_page_viewmodel.dart';
 import 'package:rcmasbusapp/ui/login/login_page_viewmodel.dart';
 
@@ -8,27 +10,33 @@ class DrawerWidget extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final pass = useProvider(busPassProvider);
-    return pass.when(
-        data: (value) => value.isApproved!
-            ? Drawer(
-                child: Column(
-                  children: [
-                    Spacer(),
-                    Divider(),
-                    ListTile(
-                      onTap: () {
-                        context.read(loginViewModelProvider).signOut();
-                      },
-                      title: Text(
-                        'Sign out',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    )
-                  ],
-                ),
-              )
-            : SizedBox(),
-        loading: () => SizedBox(),
-        error: (_, __) => SizedBox());
+    final user = userContainer.resolve<LoginUser>();
+    return user.userType == 'S'
+        ? pass.when(
+            data: (value) => value.isApproved! ? drawer(context) : SizedBox(),
+            loading: () => SizedBox(),
+            error: (_, __) => SizedBox())
+        : drawer(context);
+  }
+
+  Drawer drawer(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          Spacer(),
+          Divider(),
+          ListTile(
+            onTap: () {
+              context.read(loginViewModelProvider).signOut();
+              userContainer.clear();
+            },
+            title: Text(
+              'Sign out',
+              style: TextStyle(fontSize: 16),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
