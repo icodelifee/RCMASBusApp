d1c35b310070f0215c540c73a09b6083bdc06284
icodelifee <mithunsmenon@gmail.com>
2021-04-11 13:05:29 +0530

    ui: increase font size for name in unreg students

--- lib/ui/admin/students_list/unregistered_students.dart
+++ lib/ui/admin/students_list/unregistered_students.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:rcmasbusapp/app_theme.dart';
 import 'package:rcmasbusapp/data/model/login_user.dart';
 import 'package:rcmasbusapp/ui/admin/admin_common/appbar.dart';
 import 'package:rcmasbusapp/ui/admin/students_list/students_list_viewmodel.dart';
@@ -22,16 +24,23 @@ class UnregisteredStudents extends HookWidget {
                   itemCount: students.data!.length,
                   itemBuilder: (BuildContext context, int index) {
                     return Card(
+                      elevation: 3,
+                      shape: roundedRectangleBorder,
                       child: ListTile(
-                        contentPadding: EdgeInsets.all(10),
+                        contentPadding: EdgeInsets.all(20),
                         title: Text(
-                            '${students.data![index].firstName!} ${students.data![index].lastName!}'),
+                          '${students.data![index].firstName!} ${students.data![index].lastName!}',
+                          style: TextStyle(fontSize: 18),
+                        ),
                         subtitle: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
+                            Gap(5),
                             Text(
                                 'Roll Number : ${students.data![index].rollNumber!}'),
-                            Text('Phone : ${students.data![index].phone!}')
+                            Gap(5),
+                            Text('Phone : ${students.data![index].phone!}'),
+                            Gap(5),
                           ],
                         ),
                       ),
