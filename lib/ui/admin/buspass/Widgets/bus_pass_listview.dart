0190bcfe61134611ee68a9515b392f7e91dc5db5
icodelifee <mithunsmenon@gmail.com>
2021-04-11 22:50:36 +0530

    feat: use slidable cards to approve  buspass

--- lib/ui/admin/buspass/Widgets/bus_pass_listview.dart
+++ lib/ui/admin/buspass/Widgets/bus_pass_listview.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:gap/gap.dart';
+import 'package:get/get.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:rcmasbusapp/app_theme.dart';
@@ -23,44 +25,68 @@ class BusPassListView extends HookWidget {
       return ListView.builder(
         itemCount: buspasses.length,
         itemBuilder: (BuildContext context, int index) {
-          return Card(
-              elevation: 3,
-              shape: roundedRectangleBorder,
-              child: Container(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    HeaderText(
-                      text: 'Bus Pass Id',
+          return Slidable(
+            actionPane: SlidableBehindActionPane(),
+            actionExtentRatio: 0.25,
+            secondaryActions: <Widget>[
+              IconSlideAction(
+                caption: 'Approve',
+                foregroundColor: Colors.black,
+                color: Colors.transparent,
+                iconWidget: Icon(Icons.approval),
+                onTap: () {
+                  Get.bottomSheet(Container(
+                    padding: EdgeInsets.all(20),
+                    color: Colors.white,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        
+                      ],
                     ),
-                    DataText(text: buspasses[index].passId!),
-                    Gap(8),
-                    HeaderText(
-                      text: 'Roll Number',
-                    ),
-                    DataText(text: buspasses[index].rollNo!),
-                    Gap(8),
-                    HeaderText(
-                      text: 'Pass Creation Date',
-                    ),
-                    DataText(text: getTime(buspasses[index].timestamp!)),
-                    Gap(8),
-                    HeaderText(text: 'Pass Status'),
-                    DataText(
-                      text: buspasses[index].isApproved!
-                          ? 'Approved'
-                          : 'Not Approved',
-                    ),
-                    Gap(8),
-                    HeaderText(text: 'Pass Payment Status'),
-                    DataText(
-                        text: buspasses[index].isPaymentComplete!
-                            ? 'Payed'
-                            : 'Not Payed')
-                  ],
-                ),
-              ));
+                  ));
+                },
+              ),
+            ],
+            child: Card(
+                elevation: 3,
+                shape: roundedRectangleBorder,
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      HeaderText(
+                        text: 'Bus Pass Id',
+                      ),
+                      DataText(text: buspasses[index].passId!),
+                      Gap(8),
+                      HeaderText(
+                        text: 'Roll Number',
+                      ),
+                      DataText(text: buspasses[index].rollNo!),
+                      Gap(8),
+                      HeaderText(
+                        text: 'Pass Creation Date',
+                      ),
+                      DataText(text: getTime(buspasses[index].timestamp!)),
+                      Gap(8),
+                      HeaderText(text: 'Pass Status'),
+                      DataText(
+                        text: buspasses[index].isApproved!
+                            ? 'Approved'
+                            : 'Not Approved',
+                      ),
+                      Gap(8),
+                      HeaderText(text: 'Pass Payment Status'),
+                      DataText(
+                          text: buspasses[index].isPaymentComplete!
+                              ? 'Payed'
+                              : 'Not Payed')
+                    ],
+                  ),
+                )),
+          );
         },
       );
     } else {
