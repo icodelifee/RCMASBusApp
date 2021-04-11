6e32d274fc83ec054855faf447d9f928aabdb6e6
icodelifee <mithunsmenon@gmail.com>
2021-04-11 13:14:37 +0530

    refactor: move yourpayments and change route tiles to Widgets

---
+++ lib/ui/home/Widgets/change_route_tile.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:get/get.dart';
+import 'package:rcmasbusapp/app_theme.dart';
+import 'package:rcmasbusapp/ui/pay_bus_fee/pay_bus_fee.dart';
+
+class ChangeRouteTile extends StatelessWidget {
+  const ChangeRouteTile({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80.0,
+      decoration: cardShadow,
+      margin: EdgeInsets.all(3.0),
+      child: Card(
+        elevation: 0,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(10),
+          onTap: () => Get.to(() => PayBusFeePage()),
+          child: SizedBox(
+            height: 80.0,
+            width: 150.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              // mainAxisSize: MainAxisSize.min,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Change',
+                      style: TextStyle(
+                          fontSize: 19,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF44B679)),
+                    ),
+                    Text('Route',
+                        style: TextStyle(
+                          fontSize: 19,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ],
+                ),
+                Gap(10),
+                Image.asset(
+                  'assets/change-route.png',
+                  height: 50,
+                  width: 50,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/home/Widgets/your_payment_tile.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:get/get.dart';
+import 'package:rcmasbusapp/app_theme.dart';
+import 'package:rcmasbusapp/ui/your_payments/your_payments_page.dart';
+
+class YourPaymentsTile extends StatelessWidget {
+  const YourPaymentsTile({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80.0,
+      decoration: cardShadow,
+      margin: EdgeInsets.all(3.0),
+      child: Card(
+        elevation: 0,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: InkWell(
+          borderRadius: circularBorder,
+          onTap: () => Get.to(() => YourPaymentsPage()),
+          child: SizedBox(
+            height: 80.0,
+            width: 150.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              // mainAxisSize: MainAxisSize.min,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Your',
+                      style: TextStyle(
+                          fontSize: 19,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF44B679)),
+                    ),
+                    Text('Payments',
+                        style: TextStyle(
+                          fontSize: 19,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ],
+                ),
+                Gap(10),
+                Image.asset(
+                  'assets/your-payments.png',
+                  height: 50,
+                  width: 50,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
