1e0142672d4732d87aed0a5f43035eef1ae7d11c
icodelifee <mithunsmenon@gmail.com>
2021-04-15 16:13:28 +0530

    feat: update bus model

--- lib/data/model/bus.dart
+++ lib/data/model/bus.dart
@@ -1,23 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Bus {
-  Bus({
-    this.busId,
-    this.driverId,
-    this.busLicenseNo,
-    this.busNo,
-    this.docId,
-  });
+  Bus(
+      {this.busId,
+      this.driverId,
+      this.busLicenseNo,
+      this.busNo,
+      this.docId,
+      this.fitness,
+      this.insurance,
+      this.permit,
+      this.pollution,
+      this.tax});
 
   String? busId;
   String? driverId;
   String? busLicenseNo;
   String? busNo;
   String? docId;
+  DateTime? fitness;
+  DateTime? insurance;
+  DateTime? permit;
+  DateTime? pollution;
+  DateTime? tax;
 
   factory Bus.fromJson(Map<String, dynamic> json, String docId) => Bus(
       busId: json['bus_id'],
       driverId: json['driver_id'],
       busLicenseNo: json['bus_license_no'],
       busNo: json['bus_no'],
+      fitness: (json['fitness'] as Timestamp).toDate(),
+      insurance: (json['insurance'] as Timestamp).toDate(),
+      permit: (json['permit'] as Timestamp).toDate(),
+      pollution: (json['pollution'] as Timestamp).toDate(),
+      tax: (json['tax'] as Timestamp).toDate(),
       docId: docId);
 
   Map<String, dynamic> toJson() => {
