9df4e94d3de0fa35a19cc02bc0718c0544c06f57
icodelifee <mithunsmenon@gmail.com>
2021-04-18 00:13:42 +0530

    feat(admin): implement renewals search

--- lib/ui/admin/renewals/renewals_page.dart
+++ lib/ui/admin/renewals/renewals_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:rcmasbusapp/app_theme.dart';
+import 'package:rcmasbusapp/data/model/renewal.dart';
+import 'package:rcmasbusapp/ui/admin/renewals/renewals_viewmodel.dart';
+import 'package:search_page/search_page.dart';
 import 'Widgets/approved_renewals.dart';
 import 'Widgets/not_approved_renewals.dart';
+import 'Widgets/renewal_card.dart';
 
 class RenewalsPage extends HookWidget {
   RenewalsPage({Key? key}) : super(key: key);
@@ -12,6 +17,7 @@ class RenewalsPage extends HookWidget {
   ];
   @override
   Widget build(BuildContext context) {
+    final provider = useProvider(renewalsProvider);
     return DefaultTabController(
       length: 2,
       child: Scaffold(
@@ -31,6 +37,35 @@ class RenewalsPage extends HookWidget {
                 ),
               ],
             ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            tooltip: 'Search people',
+            onPressed: provider.data == null
+                ? null
+                : () => showSearch(
+                      context: context,
+                      delegate: SearchPage<Renewal>(
+                        items: provider.data!.value,
+                        searchLabel: 'Search renewals',
+                        suggestion: Center(
+                          child: Text(
+                              'Filter renewals by roll number, first name, course'),
+                        ),
+                        failure: Center(
+                          child: Text('No renewals found :('),
+                        ),
+                        filter: (renewal) => [
+                          renewal.rollNo,
+                          renewal.student!.firstName,
+                          renewal.student!.course,
+                        ],
+                        builder: (renewal) => RenewalsCard(
+                          approval: renewal.isApproved!,
+                          renewals: renewal,
+                        ),
+                      ),
+                    ),
+            child: Icon(Icons.search),
           ),
           body: TabBarView(
             children: pages,
