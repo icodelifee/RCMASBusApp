2d941619ddd68e6c12c2c9b183866568bb7d0c7e
icodelifee <mithunsmenon@gmail.com>
2021-04-17 23:49:49 +0530

    feat: add pass, student & payment model in renewal model

--- lib/data/model/renewal.dart
+++ lib/data/model/renewal.dart
@@ -1,4 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rcmasbusapp/data/model/bus_pass.dart';
+import 'package:rcmasbusapp/data/model/payment.dart';
+import 'package:rcmasbusapp/data/model/student.dart';
 
 class Renewal {
   Renewal(
@@ -8,6 +11,9 @@ class Renewal {
       this.renewalId,
       this.rollNo,
       this.isApproved,
+      this.payment,
+      this.pass,
+      this.student,
       this.docId});
 
   String? passId;
@@ -17,15 +23,22 @@ class Renewal {
   bool? isApproved;
   String? rollNo;
   String? docId;
+  Payment? payment;
+  BusPass? pass;
+  Student? student;
 
-  factory Renewal.fromJson(Map<String, dynamic> json, String docid) => Renewal(
-      passId: json['pass_id'],
-      paymentId: json['payment_id'],
-      renewalDate: json['renewal_date'],
-      renewalId: json['renewal_id'],
-      rollNo: json['roll_no'],
-      isApproved: json['is_approved'],
-      docId: docid);
+  factory Renewal.fromJson(
+    Map<String, dynamic> json,
+    String docid,
+  ) =>
+      Renewal(
+          passId: json['pass_id'],
+          paymentId: json['payment_id'],
+          renewalDate: json['renewal_date'],
+          renewalId: json['renewal_id'],
+          rollNo: json['roll_no'],
+          isApproved: json['is_approved'],
+          docId: docid);
 
   Map<String, dynamic> toJson() => {
         'pass_id': passId,
