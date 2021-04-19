8f89bf7a8c0a76747823238e838f131c7cf9e7c3
icodelifee <mithunsmenon@gmail.com>
2021-04-19 14:55:15 +0530

    ui: change appbar color of tabbar in buspass page

--- lib/ui/admin/buspass/buspass_page.dart
+++ lib/ui/admin/buspass/buspass_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rcmasbusapp/app_theme.dart';
 import 'package:rcmasbusapp/ui/admin/buspass/approved_buspass_page.dart';
 import 'package:rcmasbusapp/ui/admin/buspass/unapproved_buspass_page.dart';
 
@@ -9,8 +10,10 @@ class BusPassPage extends StatelessWidget {
       length: 2,
       child: Scaffold(
           appBar: AppBar(
+            backgroundColor: Colors.white,
             title: Text('Bus Passes'),
             bottom: TabBar(
+              indicatorColor: AppColors.secondaryColor,
               labelColor: Colors.black,
               tabs: [
                 Tab(
