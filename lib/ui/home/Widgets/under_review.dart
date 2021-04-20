539ace22b28e9931ce236a1354266388fea193ee
icodelifee <mithunsmenon@gmail.com>
2021-04-21 05:04:42 +0530

    feat: add signout btn in underreview widget

--- lib/ui/home/Widgets/under_review.dart
+++ lib/ui/home/Widgets/under_review.dart
@@ -3,11 +3,14 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:rcmasbusapp/app_theme.dart';
+import 'package:rcmasbusapp/data/containers/user_container.dart';
 import 'package:rcmasbusapp/data/model/bus_pass.dart';
 import 'package:rcmasbusapp/ui/home/home_page_viewmodel.dart';
+import 'package:rcmasbusapp/ui/login/login_page_viewmodel.dart';
 
 class UnderReviewWidget extends HookWidget {
   const UnderReviewWidget({
@@ -38,24 +41,38 @@ class UnderReviewWidget extends HookWidget {
                         child: Container(
                           padding: EdgeInsets.all(20),
                           width: Get.size.width,
-                          height: 200,
                           child: Column(
+                            mainAxisSize: MainAxisSize.min,
                             crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: [
                               Text(
                                 'Your bus pass request is under review, Please contact the administator if you have any queries regarding this process.',
                                 style: TextStyle(fontSize: 19),
                               ),
+                              Gap(20),
                               TextButton(
-                                  style: ButtonStyle(
-                                    shape: MaterialStateProperty.all(
-                                        RoundedRectangleBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(8))),
-                                    backgroundColor: MaterialStateProperty.all(
-                                        AppColors.buttonColor),
-                                  ),
+                                  style: buttonStyle,
+                                  onPressed: () {
+                                    context
+                                        .read(loginViewModelProvider)
+                                        .signOut();
+                                    userContainer.clear();
+                                  },
+                                  child: SizedBox(
+                                    height: 30,
+                                    width: Get.size.width,
+                                    child: Center(
+                                      child: Text(
+                                        'Sign Out',
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 16),
+                                      ),
+                                    ),
+                                  )),
+                              Gap(10),
+                              TextButton(
+                                  style: buttonStyle,
                                   onPressed: () {
                                     SystemNavigator.pop();
                                   },
